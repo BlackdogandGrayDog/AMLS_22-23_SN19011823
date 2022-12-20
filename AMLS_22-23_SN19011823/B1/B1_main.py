Correct some calculation error on number of neighbours of KNN model

AMLS_22-23_SN19011823/B1/B1_main.py
@@ -31,7 +31,7 @@ def feature_training_knn_svm(detector, predictor, basedir, labels_filename, imag
     print('\n')
     acc = np.array(acc)
     posi = np.where(acc == max(acc))
-    posi = np.array(posi) * 5 ## since the line space of number of k is 5, therefore position need to be times by 5
+    posi = (np.array(posi) * 5) + 1 ## since the line space of number of k is 5, therefore position need to be times by 5, 1 becasue k starts from 1
     print('With the number of neighbours of:')
     print(posi)
     print('The accuracy vs. Number of neighbours plot is:')
@@ -86,7 +86,7 @@ def train_and_test_knn_svm(basedir, images_dir, labels_filename, testdir, test_i
     print(max(acc))
     acc = np.array(acc)
     posi = np.where(acc == max(acc))
-    posi = np.array(posi) * 5 ## since the line space of number of k is 5, therefore position need to be times by 5
+    posi = (np.array(posi) * 5) + 1  ## since the line space of number of k is 5, therefore position need to be times by 5, 1 becasue k starts from 1
     print('With the number of neighbours of:')
     print(posi)
     print('\n')

AMLS_22-23_SN19011823/B2/B2_main.py
@@ -59,7 +59,7 @@ def KNN_training(basedir, labels_filename, eye_dir):
     print(max(KNN_accuracy))
     print('with the number of neighbours:')
     position = np.where(KNN_accuracy == max(KNN_accuracy))
-    position = np.array(position) * 5 ## since the line space of number of k is 5, therefore position need to be times by 5
+    position = (np.array(position) * 5) + 1 ## since the line space of number of k is 5, therefore position need to be times by 5, +1 becasue k starts from 1
     print(position)
     print('\n')
     print('Here is the plot of accuracy versus number of neighbours:')